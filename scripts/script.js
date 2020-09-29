ec369d8e90512573d48a732fc70b52760a508411
Mariya Lavrenchenko <mlavrencheko@yandex.ru>
2020-09-29 12:44:20 +0300

    feat: add media for footer & headder & part of content

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 console.log('Hello, world');
 
+const buttonOpenPopup = document.querySelector('.edit-button');
+const buttonClosePopup = document.querySelector('.popup__close-btn');
+const popup = document.querySelector('.popup');
+
+
+const popupToggle = () => {
+  popup.classList.toggle('popup_opened');
+}
+
+buttonOpenPopup.addEventListener('click', popupToggle);
+buttonClosePopup.addEventListener('click', popupToggle);
